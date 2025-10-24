00aeba81874779b5dde303e4a49ff541376add54
Serhii Kulbashnyi <112683442+Eugensson@users.noreply.github.com>
2025-10-24 10:12:08 +0300

    Initialized project, added assets and styles, installed npm packages

---
+++ app/layout.tsx
@@ -0,0 +1,32 @@
+import type { Metadata } from "next";
+import { Archivo } from "next/font/google";
+
+import "./globals.css";
+
+const archivo = Archivo({
+  display: "swap",
+  weight: "variable",
+  subsets: ["latin"],
+  variable: "--font-archivo",
+});
+
+export const metadata: Metadata = {
+  title: {
+    default: "Alex Taylor | Web Developer & Designer Portfolio",
+    template: "%s | Alex Taylor Portfolio",
+  },
+  description:
+    "Discover the creative work of Alex Taylor — a professional web developer and designer specializing in modern, responsive websites and user-friendly digital experiences. Explore projects, design concepts, and development expertise in one place.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${archivo.variable} antialiased`}>{children}</body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div className="text-6xl font-bold text-red-orange-500">
+      <div className="container">homepage</div>
+    </div>
+  );
+};
+
+export default Home;
